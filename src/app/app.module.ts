e7e1bb9c910f1b181446907bf38f09920955dfcc
Eduardo May <eduardomay_20400@outlook.com>
2019-01-20 15:55:55 -0500

    instalando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Components
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './components/user/profile/profile.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { DetailNewComponent } from './components/detail-new/detail-new.component';
+import { HomeComponent } from './components/home/home.component';
+import { Page404Component } from './components/page404/page404.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { ListNewsComponent } from './components/admin/list-news/list-news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProfileComponent,
+    RegisterComponent,
+    LoginComponent,
+    DetailNewComponent,
+    HomeComponent,
+    Page404Component,
+    AdminComponent,
+    ListNewsComponent
   ],
   imports: [
     BrowserModule
