be63060b0e6d30ddeafe9b8c044faf4598f15dd4
Eduardo May <eduardomay_20400@outlook.com>
2019-01-23 21:24:02 -0500

    corrigiendo las rutas

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,12 +4,16 @@ import { HomeComponent } from '../components/home/home.component';
 import { NewsComponent } from '../components/news/news.component';
 import { Page404Component } from '../components/page404/page404.component';
 import { LoginComponent } from '../components/user/login/login.component';
+import { RegisterComponent } from '../components/user/register/register.component';
+import { ProfileComponent } from '../components/user/profile/profile.component';
 
 const ROUTES: Routes = [
   { path: '', component: HomeComponent },
   { path: 'inicio', component: HomeComponent },
   { path: 'noticias', component: NewsComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'user/login', component: LoginComponent },
+  { path: 'user/registro', component: RegisterComponent },
+  { path: 'user/perfil', component: ProfileComponent },
   { path: '**', component: Page404Component }
 ];
 
