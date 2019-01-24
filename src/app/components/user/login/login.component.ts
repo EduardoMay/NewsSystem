134ebf0f33dcb28c3632181343405303856c5621
Eduardo May <eduardomay_20400@outlook.com>
2019-01-23 21:25:08 -0500

    login

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -12,15 +15,35 @@ export class LoginComponent implements OnInit {
   public isError = false;
   public errorMes = '';
 
-  constructor() { }
+  constructor(public afAuth: AngularFireAuth,
+              private router: Router,
+              public _authService: AuthService) { }
 
   ngOnInit() {
   }
 
-  onLogin() {}
+  public onLogin(): void {
+    this._authService.loginEmailUser( this.email, this.password )
+      .then( res => {
+        console.log('Logeado por correo: ', res);
+        this.isError = false;
+        this.onLoginRedirect();
+      }).catch( error => {
+        console.log('Error al iniciar sesion por correo', error.message);
+        this.isError = true;
+        setTimeout(() => {
+          this.isError = false;
+        }, 3000);
+        this.errorMes = 'Correo o Password incorrectos';
+      });
+  }
 
   onLoginFacebook() {}
 
   onLoginGoogle() {}
+
+  public onLoginRedirect() {
+    this.router.navigate(['inicio']);
+  }
 
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private _afService: AngularFireAuth) { }
+
+  public registerUser() {}
+
+  public loginEmailUser(email: string, password: string) {
+    return new Promise( (resolve, reject) => {
+      this._afService.auth.signInWithEmailAndPassword(email, password)
+        .then( userData => resolve(userData), err => reject(err));
+    });
+  }
+
+  public loginFacebook() {}
+
+  public loginGoogle() {}
+
+  public logoutUser() {}
+
+  public isAuth() {}
 }
