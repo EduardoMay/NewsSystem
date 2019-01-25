f7361ff2af81a61042d231fb6df8739fc31527d5
Eduardo May <eduardomay_20400@outlook.com>
2019-01-25 14:17:18 -0500

    mostrando informacion en la pagina de perfil

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/service/auth.service';
+import { UserInterface } from 'src/app/models/user';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  public user: UserInterface = {
+    name: '',
+    email: '',
+    photoUrl: ''
+  };
+  public providerId = 'null';
+
+  constructor(private _authService: AuthService) { }
 
   ngOnInit() {
+    this._authService.isAuth().subscribe( userData => {
+      if ( userData ) {
+        this.user.name = userData.displayName;
+        this.user.email = userData.email;
+        this.user.photoUrl = userData.photoURL;
+        this.providerId = userData.providerData[0].providerId;
+        console.log('Datos del usuario: ', this.providerId);
+      }
+    });
   }
 
 }
