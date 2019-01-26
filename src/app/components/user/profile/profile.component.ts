739d147100b47dbfe336de24421c56aea021d5a3
Eduardo May <eduardomay_20400@outlook.com>
2019-01-25 19:21:11 -0500

    subir foto de perfil angular storage

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { AuthService } from 'src/app/service/auth.service';
 import { UserInterface } from 'src/app/models/user';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
@@ -9,6 +12,9 @@ import { UserInterface } from 'src/app/models/user';
 })
 export class ProfileComponent implements OnInit {
 
+  public divUpdatePhoto = false;
+  public alertMessage = '';
+  public alertAct = false;
   public user: UserInterface = {
     name: '',
     email: '',
@@ -16,7 +22,12 @@ export class ProfileComponent implements OnInit {
   };
   public providerId = 'null';
 
-  constructor(private _authService: AuthService) { }
+  public uploadPercent: Observable<number>;
+  public urlImage: Observable<string>;
+  @ViewChild('imageUser') inputImageUser: ElementRef;
+
+  constructor(private _authService: AuthService,
+              private angStorage: AngularFireStorage) { }
 
   ngOnInit() {
     this._authService.isAuth().subscribe( userData => {
@@ -28,6 +39,46 @@ export class ProfileComponent implements OnInit {
         console.log('Datos del usuario: ', this.providerId);
       }
     });
+  }
+
+  public uploadImagenUser() {
+    this._authService.isAuth().subscribe( userData => {
+      if ( userData ) {
+        userData.updateProfile({
+          displayName: '',
+          photoURL: this.inputImageUser.nativeElement.value
+        }).then( () => {
+          console.log('Usuario actualizado');
+          // Alerta
+          this.alertMessage = 'Has actualizado tus datos';
+          this.alertAct = true;
+          // Esconder el cuadro para subir foto
+          this.divUpdatePhoto = false;
+          setTimeout(() => {
+            this.alertAct = false;
+          }, 3000);
+        }).catch( () => {
+          console.log('Error al actualizar perfil');
+        });
+      }
+    });
+  }
+
+  public onUpload(e) {
+    const id = Math.random().toString(36).substring(2);
+    const file = e.target.files[0];
+    const filePath = `uploads/profile_${id}`;
+    const ref = this.angStorage.ref(filePath);
+    const task = this.angStorage.upload(filePath, file);
+
+    this.uploadPercent = task.percentageChanges();
+    task.snapshotChanges().pipe( finalize( () => {
+      this.urlImage = ref.getDownloadURL();
+    })).subscribe();
+  }
+
+  public btnUpdatePhoto() {
+    this.divUpdatePhoto = true;
   }
 
 }
