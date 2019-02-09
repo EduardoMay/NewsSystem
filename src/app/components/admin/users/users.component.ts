96c00cbf3e0f1ee59874688e02d865a266aa9379
Eduardo May <eduardomay_20400@outlook.com>
2019-02-09 11:46:45 -0500

    documentando

--- src/app/components/admin/users/users.component.ts
+++ src/app/components/admin/users/users.component.ts
@@ -1,3 +1,18 @@
+/**
+ * @fileoverview UsersComponent, se implementa metodos para obtener todos los usuarios
+ * de la base de datos.
+ * Cambiar el role de un usuario.
+ *
+ * @version 1.0
+ *
+ * @author Eduardo May<eduardo_may@outlook.com>
+ *
+ * History
+ * v1.0 Se implemento diferentes metodos para obtener los usuarios y cambiar el tipo de usuario
+ *
+ * La primara version de UsersComponent fue escrita por Eduardo May
+*/
+
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { UsersApiService } from 'src/app/service/users-api.service';
 import { UserInterface } from 'src/app/models/user';
@@ -10,18 +25,21 @@ import { UserInterface } from 'src/app/models/user';
 export class UsersComponent implements OnInit {
 
   @ViewChild('btnClose') btnClose: ElementRef;
-  public users: UserInterface[] = [];
-  public role = '';
+  public users: UserInterface[] = []; // almacena todos los usuarios
+  public role = ''; // almacena el nuevo rol
 
-  public userUid = '';
-  public nameUser = '';
+  public userUid = ''; // almacena el id del usuario seleccionado
+  public nameUser = ''; // almacena el nombre del usuario del usuario seleccionado
 
   constructor(private _usersApiService: UsersApiService) { }
 
   ngOnInit() {
-    this.getListUsers();
+    this.getListUsers(); // obtiene todos los usuarios
   }
 
+  /**
+   * obtiene todos los usuarios
+  */
   public getListUsers() {
     this._usersApiService.getAllUsers().subscribe( users => {
       this.users = users;
@@ -29,12 +47,19 @@ export class UsersComponent implements OnInit {
     });
   }
 
-  public getDataUser(id: string, name: string){
+
+  /**
+   * obtiene los datos del usuario seleccionado
+  */
+  public getDataUser(id: string, name: string) {
     this.userUid = id;
     this.nameUser = name;
     console.log(this.userUid);
   }
 
+  /**
+   * actualiza el role del usuario seleccionado
+  */
   public updateUser() {
     console.log(this.role);
     if (this.role === 'miembro') {
