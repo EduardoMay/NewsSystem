6b0cc88c4079fc781cc1b6fe36a1640e7da9d5b1
Eduardo May <eduardomay_20400@outlook.com>
2019-03-22 18:36:53 -0500

    agregando componentes de angular material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,17 +4,23 @@ import { CommonModule } from '@angular/common';
 // material
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTableModule,
+    MatFormFieldModule
   ],
   exports: [
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTableModule,
+    MatFormFieldModule
   ]
 })
 export class MaterialModule { }
